eab9a1ef4951230b9e130cd8ea2c9587d9a60dff
vmouchakis <vmouchakis@gmail.com>
2022-06-06 10:02:00 +0300

    comments added, readme file added, chnged the theme of the page (colors)

--- app/downloader.py
+++ app/downloader.py
@@ -1,24 +1,18 @@
-from tracemalloc import start
 import requests
-from pprint import PrettyPrinter
 import pandas as pd
 pd.set_option('display.float_format', '{:.8f}'.format)
 from collections import defaultdict
-from datetime import datetime
 
-pp = PrettyPrinter()
-
-api_key = "DpmLXIe2jzwffE2vxsZcEp8ye0rwpiddSJrJvonu"
-# start_date = "2020-01-22"
-# end_date = "2020-01-24"
 
 class Downloader():
 
+    # create a method to send a request to the api, retrieve and manipulate the response, and show the results
     def fetch_asteroids(self, start_date, end_date):
 
-        # start_date = start_date.strftime("%Y-%m-%d")
-        # end_date = end_date.strftime("%Y-%m-%d")
-
+        # initializing the appropriate  parameters for the request
+        # the 'url' and its parapemeters were retrieved from the documentation of the NASA api
+        # as parameters we need the 'start' and 'end date' and the 'api key'
+        # the 'detailed' parameter is optional and it enables a more detailed response from the api
         url = "https://api.nasa.gov/neo/rest/v1/feed"
         params = {
             "api_key":"DpmLXIe2jzwffE2vxsZcEp8ye0rwpiddSJrJvonu",
@@ -27,40 +21,41 @@ class Downloader():
             "detailed":"true"
         }
         response = requests.get(url,params=params).json()
-        # pp.pprint(response)
-        # return response
 
-        # dl = Downloader()
-        # data = dl.fetch_asteroids(api_key, start_date, end_date)
+        # check if the api found at least one object
+        # if not, we return '0' (we 'll check if this class returns a datframe or not)
+        if response["element_count"]  == 0:
+            return 0
 
+        # save the objects in a list for easier manipulation
         neo = response["near_earth_objects"]
 
+        # saving the dates in a list (dates are specified from the user)
         dates = []
-
         for date in neo:
             dates.append(date)
 
-
+        # create a temp list to save the objects of each date
         asteroids_temp = []
-
         for date in dates:
             
             for asteroid in neo[date]:
                 asteroids_temp.append(asteroid)
 
-
+        # create a dict to save the desired attributes for each object
         asteroids = defaultdict(dict)
-
         for asteroid in asteroids_temp:
+            # for each object we save its id, its name, it's closets distance to earth in km, the date of its closest distance to earth
+            # and its size, to calculate its size, we find the mean of its max and min diameters in km, as the objects usually don't have a spherical shape
             asteroids[asteroid["id"]] = {"name": [], "approach_distance (km)": [], "approach_date": [], "size (km)": []}
             asteroids[asteroid["id"]]["name"] = asteroid["name"]
-            asteroids[asteroid["id"]]["approach_distance (km)"] = asteroid["close_approach_data"][0]["miss_distance"]["kilometers"]
-            asteroids[asteroid["id"]]["approach_date"] = asteroid["close_approach_data"][0]["close_approach_date"]
             asteroids[asteroid["id"]]["size (km)"] = (asteroid["estimated_diameter"]["kilometers"]["estimated_diameter_max"] + asteroid["estimated_diameter"]["kilometers"]["estimated_diameter_min"]) / 2
+            asteroids[asteroid["id"]]["approach_date"] = asteroid["close_approach_data"][0]["close_approach_date"]
+            asteroids[asteroid["id"]]["approach_distance (km)"] = asteroid["close_approach_data"][0]["miss_distance"]["kilometers"]
+            
 
-
+        # create a dataframe out of the previous dict and sort values by the 'approach distance' ascending
         df = pd.DataFrame.from_dict(asteroids).T
         df["approach_distance (km)"] = df["approach_distance (km)"].astype(float)
         df.sort_values(by=["approach_distance (km)"], inplace=True)
-        # pp.pprint(df)
-        return df.style.hide_index()
+        return df
--- app/main.py
+++ app/main.py
@@ -1,47 +1,103 @@
 from datetime import date
-from pprint import PrettyPrinter
-import string
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from requests import request
 from app.downloader import Downloader
-from datetime import datetime
+from typing import Optional
+from pathlib import Path
+import pandas as pd
 
-
+# Initializing the application
 app = FastAPI()
 
+# setting the path for static files (for css files)
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"),
+    name="static",
+)
+
+# setting the path for templates (for html files)
 templates = Jinja2Templates(directory="templates")
 
-
+# get request to load the start page where the user can insert the desired dates
 @app.get("/")
 async def get_dates(request: Request):
-    data = "Test"
+    data = ""
     return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
+# post method to retrieve the user input and make the request to the NASA api 
 @app.post("/")
-async def get_data(request: Request, start_date: date = Form(...), end_date: date = Form(...)):
-    start_date = start_date.strftime("%Y-%m-%d")
-    end_date = end_date.strftime("%Y-%m-%d")
-    dl = Downloader()
-    data = dl.fetch_asteroids(start_date=start_date, end_date=end_date)
-    print(data)
-    return templates.TemplateResponse("page.html", {"request": request, "data": data.to_html()})
+async def get_data(request: Request, start_date: date = Form(default=""), end_date: Optional[date] = Form(default="")):
+    # for both 'start' and 'end' dates we set the default value to an empty string, as it is easy to catch errors
+
+    # check if the user inserted a value for the 'start date'
+    if start_date == "":
+        # if the user does not set a 'start date' we use as 'start date' the current date and as an 'end date' an empty string
+        # if the api does not find an 'end date', it sets the 'end date' for seven days after the 'start date'
+
+        today = date.today()
+        start_date = today.strftime("%Y-%m-%d")
+        end_date = ""
+
+        # create an object of Downloader class
+        # and pass the values
+        dl = Downloader()
+        data = dl.fetch_asteroids(start_date=start_date, end_date=end_date)
+        print(data)
+
+        # check if the Downloader class returns a dataframe with the desired objects, or not
+        if isinstance(data, pd.DataFrame):
+            # if the Downloader returns a dataframe, meaning the desired objects, we return it as an html table
+            return templates.TemplateResponse("page.html", {"request": request, "data": data.to_html(index=False, justify="center")})
+        else:
+            # if it is not, we return a message that no objects were found
+            data = "0 objects found."
+            return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+    # after checking for the 'start date', we check again for the 'end date'
+    if end_date == "":
+        # if the api does not find an 'end date', it sets the 'end date' for seven days after the 'start date'
+        # so we do not change the value of the 'end date'
+
+        start_date = start_date.strftime("%Y-%m-%d")
+
+        # create an object of Downloader class
+        # and pass the values
+        dl = Downloader()
+        data = dl.fetch_asteroids(start_date=start_date, end_date=end_date)
+        print(data)
+
+        # check if the Downloader class returns a dataframe with the desired objects, or not
+        if isinstance(data, pd.DataFrame):
+            # if the Downloader returns a dataframe, meaning the desired objects, we return it as an html table
+            return templates.TemplateResponse("page.html", {"request": request, "data": data.to_html(index=False, justify="center")})
+        else:
+            # if it is not, we return a message that no objects were found
+            data = "0 objects found."
+            return templates.TemplateResponse("page.html", {"request": request, "data": data})
+    else:
+        # if the user correctly inserted both 'start' and 'end date', then we pass these values to the Downloader to make the request to the api
+
+        start_date = start_date.strftime("%Y-%m-%d")
+        end_date = end_date.strftime("%Y-%m-%d")
+
+        # create an object of Downloader class
+        # and pass the values
+        dl = Downloader()
+        data = dl.fetch_asteroids(start_date=start_date, end_date=end_date)
+        print(data)
+        
+        # check if the Downloader class returns a dataframe with the desired objects, or not
+        if isinstance(data, pd.DataFrame):
+            # if the Downloader returns a dataframe, meaning the desired objects, we return it as an html table
+            return templates.TemplateResponse("page.html", {"request": request, "data": data.to_html(index=False, justify="center")})
+        else:
+            # if it is not, we return a message that no objects were found
+            data = "0 objects found."
+            return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
-
-# @app.get("/", response_class=HTMLResponse)
-# async def home(request: Request):
-#     data = {
-#         "page": "Home page"
-#     }
-#     return templates.TemplateResponse("page.html", {"request": request, "data": data})
-
-
-# @app.get("/page/{page_name}", response_class=HTMLResponse)
-# async def page(request: Request, page_name: str):
-#     data = {
-#         "page": page_name
-#     }
-#     return templates.TemplateResponse("page.html", {"request": request, "data": data})
